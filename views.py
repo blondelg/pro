562a8908d23addff0c1b0ff99d8e8a6b6f1c852f
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-04-08 15:57:02 +0200

    add comming soon page

--- views.py
+++ views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     """ Display home page """
-    return render(request, 'home.html')
+    return render(request, 'coming_soon.html')
