24886ac66b12e8fc08c284cf82163d1c47cf1d55
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-04-12 11:14:56 +0200

    disable color background of nevbar elments in mobile mode

--- models.py
+++ models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Experience(models.Model):
+    exp_compagny_name =
+    exp_geo =
+    exp_start =
+    exp_stop =
+
+class Projet(models.Model):
+    pass
+
+class Skill(models.Model):
+    ski_name =
+    ski_level =
+    ski_type =
+
+class Study(models.Model):
+    pass
+
+class Image(models.Model):
+    pass
+
+class SkillType(models.Model):
+    pass
