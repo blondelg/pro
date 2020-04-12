9c64371021acedccf0b4c32328f52129b0ffe4b0
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-04-12 18:29:49 +0200

    update EXP background, models and home template

--- models.py
+++ models.py
@@ -1,19 +1,36 @@
 from django.db import models
 
+ORDER = [(a,a) for a in range(1,21)]
+
 # Create your models here.
+class About(models.Model):
+    abo_icon = models.CharField(max_length=60)
+    abo_title = models.CharField(max_length=30)
+    abo_description = models.TextField()
+    abo_order = models.IntegerField(choices=ORDER, default=20)
+    abo_display = models.BooleanField(default=False)
+
 class Experience(models.Model):
-    exp_compagny_name =
-    exp_geo =
-    exp_start =
-    exp_stop =
+    exp_compagny_name = models.CharField(max_length=30)
+    exp_geo = models.CharField(max_length=30)
+    exp_start = models.DateField()
+    exp_stop = models.DateField()
+    exp_order = models.IntegerField(choices=ORDER, default=20)
+    exp_display = models.BooleanField(default=False)
 
 class Projet(models.Model):
-    pass
+    pro_title = models.CharField(max_length=30)
+    pro_year = models.IntegerField(default=2020)
+    pro_description = models.TextField()
+    pro_order = models.IntegerField(choices=ORDER, default=20)
+    pro_display = models.BooleanField(default=False)
 
 class Skill(models.Model):
     ski_name =
     ski_level =
     ski_type =
+    ski_order = models.IntegerField(choices=ORDER, default=20)
+    ski_display = models.BooleanField(default=False)
 
 class Study(models.Model):
     pass
